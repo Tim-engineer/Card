26ab7b480afd0dde33838389335efe7db5586dd6
timotej.matlak <timotej.matlak@wbpo.sk>
2025-11-01 08:14:47 +0100

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Tim on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
